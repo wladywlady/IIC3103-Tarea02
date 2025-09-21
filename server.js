79e075db503007b317ae0cbf82983e09ef7645c3
wladym3 <wlady_vbs@hotmail.com>
2025-09-21 18:37:48 -0300

    deploy to render

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Servir archivos estáticos desde el directorio 'public'
 app.use(express.static(path.join(__dirname, 'public')));
